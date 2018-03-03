65edc61cc1957850b489c80c21e1a85553d3f6a1
s14222 <s14222@pjwstk.edu.pl>
2018-03-03 13:46:07 +0100

    s14222 init

---
+++ src/main/java/com/s14222/tau/finder/Trup.java
@@ -0,0 +1,13 @@
+package com.s14222.tau.finder;
+
+public class Trup {
+    public int zycie;
+    public int sila;
+    public String nazwa;
+    public void przedstawSie() {
+        System.out.println(nazwa);
+    }
+    public void aktywujSuperMoc() {
+        sila += 3;
+    }
+}
---
+++ src/test/java/com/s14222/tau/finder/TrupTest.java
@@ -0,0 +1,13 @@
+package com.s14222.tau.finder;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import org.junit.*;
+import com.s14222.tau.finder.*;
+public class TrupTest {
+    @Test
+    public void KiedySuperMocAktywowana_ToZwiekszSileO3() {
+      assertNotNull(new Trup());
+    }
+}
