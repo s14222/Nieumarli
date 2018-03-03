c0942863c263e46f79377502d88fd617816dad86
s14222 <s14222@pjwstk.edu.pl>
2018-03-03 14:19:54 +0100

    Add unit test to supermoc

--- src/test/java/com/s14222/tau/finder/TrupTest.java
+++ src/test/java/com/s14222/tau/finder/TrupTest.java
@@ -7,7 +7,18 @@ import org.junit.*;
 import com.s14222.tau.finder.*;
 public class TrupTest {
     @Test
+    public void testTrupa() {
+      Trup trup = new Trup();
+      trup.sila = 3;
+      trup.aktywujSuperMoc();
+      assertNotNull(trup);
+    }
+
+    @Test
     public void KiedySuperMocAktywowana_ToZwiekszSileO3() {
-      assertNotNull(new Trup());
+      Trup trup = new Trup();
+      trup.sila = 3;
+      trup.aktywujSuperMoc();
+      assertEquals(trup.sila, 6);
     }
 }
